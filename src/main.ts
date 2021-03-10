13da917f971bee555bbbd9962519f458cb12af39
pizzaluc <lucasv9@live.dk>
2021-03-10 14:49:30 +0100

    Feature(frontend) Rest Api

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-
+import axios from 'axios'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+  el: '#app',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://localhost:5001/')
+      .then(response => (console.log(response.data)))
+  }
 }).$mount('#app')
