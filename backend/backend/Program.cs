65afd0309c80fc1ef077656c742134bc00bb22f2
Smilla <smdi@itu.dk>
2021-03-11 17:35:26 +0100

    Feature(frontend): Display JSON on webpage

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -14,50 +14,44 @@ namespace backend
 {
     class Program
     {
-
-        private static readonly Uri Nats = new Uri("nats://sysadmin:zZn6MvjhbSP8RG9f@nats1.westeurope.cloudapp.azure.com:4222/");
+        private static readonly Uri Nats =
+            new Uri("nats://sysadmin:zZn6MvjhbSP8RG9f@nats1.westeurope.cloudapp.azure.com:4222/");
 
         //private static readonly List<Type> types = new List<Type>() { typeof(Server), typeof(Connection) };
-        
+
         public static List<Server> Servers = new List<Server>();
- 
-        static void Main(string[] args)
+
+        private static void Main(string[] args)
         {
             var options = ConnectionFactory.GetDefaultOptions();
             options.Url = Nats.OriginalString;
 
-            using (var connection = new ConnectionFactory().CreateConnection(options)) 
+            using (var connection = new ConnectionFactory().CreateConnection(options))
             {
                 Console.WriteLine(connection.State);
 
                 var inbox = connection.NewInbox();
-                
+
                 using (var subscription = connection.SubscribeAsync(inbox, IncomingMessageHandler))
-                    {
+                {
                     subscription.Start();
                     connection.Publish("$SYS.REQ.SERVER.PING.VARZ", inbox, new byte[0]);
                     //connection.Publish("$SYS.REQ.SERVER.PING.CONNZ", inbox, new byte[0]);
                     Thread.Sleep(TimeSpan.FromSeconds(5));
-                    }      
-
+                }
             }
 
             CreateHostBuilder(args).Build().Run();
-            
         }
 
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
 
 
-
-
-        private static void IncomingMessageHandlerRawJson(object sender, MsgHandlerEventArgs e){
+        private static void IncomingMessageHandlerRawJson(object sender, MsgHandlerEventArgs e)
+        {
             var json = JsonConvert.DeserializeObject(Encoding.UTF8.GetString(e.Message.Data));
 
             Console.WriteLine(e.Message.ArrivalSubscription.Subject);
@@ -71,11 +65,10 @@ namespace backend
 
         private static void IncomingMessageHandler(object sender, MsgHandlerEventArgs e)
         {
-
             Console.WriteLine(e.Message.ToString());
 
             var json = JsonConvert.DeserializeObject(Encoding.UTF8.GetString(e.Message.Data));
-            
+
             JToken token = JObject.Parse(json.ToString());
 
             var data_json = token.SelectToken("data");
@@ -83,9 +76,12 @@ namespace backend
 
             Server server = new Server();
 
-            try {
-            server = JsonConvert.DeserializeObject<Server>(data_json.ToString());
-            } catch (Exception x) {
+            try
+            {
+                server = JsonConvert.DeserializeObject<Server>(data_json.ToString());
+            }
+            catch (Exception x)
+            {
                 Console.WriteLine(x.StackTrace);
             }
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 <template>
 <div id="app">
-  {{ info }}
+  Hello World!
 </div>
 </template>
-
