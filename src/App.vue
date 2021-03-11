b9231846bd67be3bd68bef53d606ec3fa4831ec6
Smilla <smdi@itu.dk>
2021-03-11 17:35:26 +0100

    Feature(frontend): Display JSON on webpage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,28 @@
 <template>
 <div id="app">
-  {{ info }}
+  <p>Data result: {{ JSON.stringify(dataNATS) }}</p>
 </div>
 </template>
 
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  props: {
+    result: Object
+  },
+  data () {
+    return {
+      dataNATS: null // JSON object containing our NATS data
+    }
+  },
+  mounted () {
+    axios
+      .get('https://localhost:5001/')
+      .then(response => {
+        this.dataNATS = response.data
+      })
+  }
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
 import Vue from 'vue'
-import axios from 'axios'
+
+import router from './router'
+import App from './App.vue'
+
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+
 new Vue({
-  el: '#app',
-  data () {
-    return {
-      info: null
-    }
-  },
-  mounted () {
-    axios
-      .get('https://localhost:5001/')
-      .then(response => (console.log(response.data)))
-  }
+  router,
+  render: createElement => createElement(
+    App
+  )
 }).$mount('#app')
