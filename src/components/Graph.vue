b73f580e67080e1aa32219cf804ae206cbfc139c
Smilla <smdi@itu.dk>
2021-03-23 15:41:05 +0100

    Add property for routes

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,24 +1,17 @@
 ﻿<template>
   <div id="graph">
-    <p id="test">Color test</p>
-    <ul id="example-1">
-      <li v-for="server in dataNATS" :key="server[0]">
-        {{ server.server_name }}
-      </li>
-    </ul>
+    <p>Hello</p>
   </div>
 </template>
 
 <script>
-import * as d3 from 'd3'
+// import * as d3 from 'd3'
 
 export default {
   name: 'Graph',
   props: {
-    dataNATS: Object
-  },
-  mounted () {
-    d3.select('#test').style('color', 'green')
+    servers: Object,
+    routes: Object
   }
 }
 </script>
