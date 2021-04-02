5599b21c13d43a98d2b5080b1c083885c9e7e1fe
Smilla <smdi@itu.dk>
2021-04-02 16:47:26 +0200

    Add search for server name (links broken)

---
+++ src/components/Searchbar.vue
@@ -0,0 +1,22 @@
+﻿<template>
+<div id='searchbar'>
+  <input id='search-bar' @input="onSearch" class="form-control" type="text" placeholder="Search" aria-label="Search"/>
+</div>
+</template>
+
+<script>
+export default {
+  computed: {
+    searchQuery: String
+  },
+  methods: {
+    onSearch () {
+      this.$emit('input', document.getElementById('search-bar').value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
