ea598bc180359eb55df70a4634b783d99197528f
maasdamind <mads.da.mind@hotmail.com>
2021-04-12 13:58:36 +0200

    Add tests and import Moq package

--- tests/backend.Tests/UnitTest1.cs
+++ tests/backend.Tests/UnitTest1.cs
@@ -1,4 +1,7 @@
+using System;
+using System.IO;
 using Xunit;
+using backend.helpers;
 
 namespace backend.Tests
 {
@@ -8,6 +11,26 @@ namespace backend.Tests
         public void Test1()
         {
             Assert.True(true);
+        }
+
+        [Fact]
+        public void EnvTest()
+        {
+            Assert.Throws<Exception>(() => DotEnv.Load(""));
+        }
+
+        [Fact]
+        public void EnvTestWorks()
+        {
+            FileStream fs = new FileStream("test.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter textWriter = new StreamWriter(fs);
+
+            textWriter.WriteLine("TESTVAR=JOHN");
+            textWriter.Close();
+
+            DotEnv.Load("test.txt");
+
+            Assert.Equal("JOHN", Environment.GetEnvironmentVariable("TESTVAR"));
         }
     }
 }
