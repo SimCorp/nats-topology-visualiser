1c5e3ac820aec06d5aab5cad7d0cc9cda52fd64e
kbjo <kbjo@itu.dk>
2021-04-16 17:54:33 +0200

    Add statusbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
-<div id="app">
-  <h1>Visualizer</h1>
-  <Graph/>
-</div>
+  <b-container class="column">
+    <div id="app" class="column">
+      <h1>Visualizer</h1>
+      <Graph/>
+    </div>
+    <div>
+      <Statusbar/>
+    </div>
+  </b-container>
 </template>
 
 <script lang="ts">
 import Graph from '@/components/Graph/Graph.vue'
+import Statusbar from '@/components/Statusbar.vue'
 
 export default {
   name: 'App',
-  components: { Graph }
+  components: { Graph, Statusbar }
 }
 
 </script>
---
+++ src/components/Statusbar.vue
@@ -0,0 +1,32 @@
+﻿<template>
+  <div id='statusbar'>
+    <p id='status-bar' class='form-control'> Last updated: {{this.timeOfReq}} </p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Statusbar",
+  async mounted () {
+    await this.getData()
+  },
+  data() {
+    return {
+      timeOfReq: String
+    }
+  },
+  methods: {
+    async getData () {
+      const timeOfRequest = await axios.get('https://localhost:5001/timeOfRequest')
+      this.timeOfReq = timeOfRequest.data.toString()
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
