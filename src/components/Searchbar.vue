187f82c423f502d9b8c4b3ce771da7925702bbff
Smilla <smdi@itu.dk>
2021-04-16 18:12:03 +0200

    Fix styling of searchbar

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -1,24 +1,35 @@
 ﻿<template>
 <div id='searchbar'>
-  <input id='search-bar' @input="onSearch" class="form-control" type="text" placeholder="Search" aria-label="Search"/>
+  <!-- <input id='search-bar' @input="onSearch" class="form-control" type="text" placeholder="Search" aria-label="Search"/> -->
+  <b-input-group class="mt-3" id='search-bar'>
+    <b-form-input v-model="text" @input="onSearch" placeholder="Search"></b-form-input>
+    <b-input-group-append>
+      <b-button variant="info">&#x2715;</b-button>
+    </b-input-group-append>
+  </b-input-group>
 </div>
 </template>
 
 <script>
 export default {
-  computed: {
-    searchQuery: String
+  data() {
+    return {
+      text: ''
+    }
   },
   methods: {
     // This method runs every time an input is given to the search bar
     // Emits the search text so other components can access it
     onSearch () {
-      this.$emit('input', document.getElementById('search-bar').value)
+      this.$emit('input', this.text)
     }
   }
 }
 </script>
 
 <style scoped>
-
+#search-bar {
+  width: 20%;
+  left: 20px;
+}
 </style>
