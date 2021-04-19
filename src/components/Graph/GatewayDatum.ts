a35e16cc83672933d8454756cfac57f6ee791b4a
Thomas Hoffmann Kilbak <thomaskilbak@gmail.com>
2021-04-19 18:29:09 +0200

    Add gateways to simulations
    Co-Authored-By: Ting Hua Lin <61328447+cupcakestudio@users.noreply.github.com>
    Co-Authored-By: Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>

---
+++ src/components/Graph/GatewayDatum.ts
@@ -0,0 +1,6 @@
+import LinkDatum from './LinkDatum'
+import ClusterDatum from './ClusterDatum'
+
+export default class GatewayDatum extends LinkDatum<ClusterDatum> {
+    
+}
---
+++ src/components/Graph/LinkDatum.ts
@@ -0,0 +1,9 @@
+import d3, { SimulationNodeDatum } from 'd3'
+
+export default class LinkDatum<T extends SimulationNodeDatum> implements d3.SimulationLinkDatum<T> {
+    source!: T;
+    target!: T;
+    ntv_error!: boolean;
+    errors!: string[];
+    errorsAsString!: string;
+}
