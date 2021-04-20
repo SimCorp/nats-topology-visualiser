4891063a03f461100eef9cfd655b38d5afffcc45
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2021-04-20 13:36:12 +0200

    Move data fetching to App, move functions to methods

--- tests/unit/graph.spec.ts
+++ tests/unit/graph.spec.ts
@@ -1,14 +1,23 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount, Wrapper } from '@vue/test-utils'
+import { CombinedVueInstance } from 'vue/types/vue'
 import Graph from '@/components/Graph/Graph.vue'
+import Methods from '@/components/Graph/Graph.vue'
 
 describe('Graph', () => {
-  const graph = shallowMount(Graph)
+  // Methods set to any because typescript can't figure it out...
+  // Maybe a fix https://github.com/vuejs/vue/issues/8721#issuecomment-545671348
+  const graph = mount<CombinedVueInstance<Graph, object, any, object, Record<never, any>>>(Graph, {})
 
-  it('Graph contains an svg', async () => {
+
+  it('Graph contains an svg', () => {
     expect(graph.find('div#visualizer').exists()).toBe(true)
 
     // TODO: test fails idk why
     // expect(graph.find('svg').exists()).toBe(true)
   })
-  
+
+  it('CalculateViewBox', () => {
+    expect(graph.vm.calculateViewBoxValue(100,50,1)).toEqual('-50, -25, 100, 50')
+  })
+
 })
