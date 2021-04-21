5ac16649a1b3676e13d6403a1913f5f7dcc7982d
Thorsten Kaae <thkj@itu.dk>
2021-04-21 14:23:13 +0200

    Make sideBar open when node is clicked
    When you click on a node the sidebar will open and
    when you click anywhere again the sidebar will close
    
    Co-authored-by: Smilla Maria Dion <smilla.dion@live.dk>

--- src/components/Graph/Graph.vue
+++ src/components/Graph/Graph.vue
@@ -261,6 +261,8 @@ export default {
         .style('opacity', d => d.isSearchMatch ? 1.0 : 0.2)
         .call(this.drag(simulation)) // Handle dragging of the nodes
         .on('click', (d, i) => { // Log the value of the chosen node on click
+          this.$emit('node-click')
+
           axios.get('https://localhost:5001/varz/' + i.server_id).then(a => {
             console.log(d)
             console.log(i)
