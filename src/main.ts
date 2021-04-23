a8e75e20bc21685f6225fc2d30480b65c8f9ef8e
kbjo <kbjo@itu.dk>
2021-04-23 10:57:03 +0200

    Zoom scrool

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,19 @@ import App from './App.vue'
 
 import BootstrapVue from 'bootstrap-vue'
 
+// @ts-ignore
+import VueZoomer from 'vue-zoomer'
+// @ts-ignore
+import Panzoom from 'vue-panzoom'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
+Vue.use(VueZoomer)
+Vue.use(Panzoom)
 
 new Vue({
   router,
