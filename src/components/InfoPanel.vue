cb0813c52bb3fb2dc57aa4af176536b0d8cef3b3
Smilla <smdi@itu.dk>
2021-04-30 16:24:54 +0200

    Add support for error nodes

--- src/components/InfoPanel.vue
+++ src/components/InfoPanel.vue
@@ -10,7 +10,10 @@
       width="400px"
       v-model="isPanelOpen">
       <div class="px-3 py-2">
-        <json-view :data="nodeData" :root-key="rootName"/>
+        <p id="error">The selected node does not exist.</p>
+        <div id="info">
+          <json-view :data="nodeData" :root-key="rootName"/>
+        </div>
       </div>
     </b-sidebar>
   </div>
@@ -34,7 +37,18 @@ export default {
       if (!this.$data.isPanelOpen) {
         this.$data.isPanelOpen = true
       }
-      this.nodeData = nodeData
+
+      const info = document.getElementById("info")
+      const error = document.getElementById("error")
+
+      if (nodeData === '') {
+        info.style.display = "none"
+        error.style.display = "block"
+      } else {
+        info.style.display = "block"
+        error.style.display = "none"
+        this.nodeData = nodeData
+      }
     }
   }
 }
